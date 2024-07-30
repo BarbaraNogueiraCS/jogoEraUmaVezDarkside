5a9eead6a2f7267668fc416a522fd9384e1301b5
Bárbara Nogueira <131182884+BarbaraNogueiraCS@users.noreply.github.com>
2024-07-29 22:22:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,248 +1,283 @@
 const questions = [
     { 
-        question: "Quem é o autor do livro Drácula?", 
+        question: "Quem é o autor do livro 'Drácula'?", 
         options: ["Mary Shelley", "Bram Stoker", "Edgar Allan Poe"], 
         correctAnswer: 1, 
         hint: "O autor é um escritor irlandês conhecido por suas histórias de horror gótico.",
-        background: "url('imagens2/img34novo.jpg')"/* img34 é capa*/ 
+        background: "url('imagens/img45.jpg')"
     },
     { 
         question: "Qual é o nome do castelo onde Drácula vive?", 
         options: ["Castelo Transilvânia", "Castelo de Harker", "Castelo de Drácula"], 
         correctAnswer: 2, 
         hint: "Este castelo está localizado na Transilvânia.",
-        background: "url('imagens2/img5.jpg')"
+        background: "url('imagens/img1.jpg')"
     },
     { 
         question: "Qual personagem inicialmente viaja para a Transilvânia para encontrar o Drácula?", 
         options: ["Dr. John Seward", "Jonathan Harker", "Arthur Holmwood"], 
         correctAnswer: 1, 
         hint: "Este personagem é um advogado inglês.",
-        background: "url('imagens2/img6.jpg')" 
+        background: "url('imagens/img2.jpg')" 
     },
     { 
         question: "Quem é a melhor amiga de Mina Harker que também se torna alvo de Drácula?", 
         options: ["Lucy Westenra", "Wilhelmina Murray", "Elizabeth Bathory"], 
         correctAnswer: 0, 
         hint: "Esta personagem é noiva de Arthur Holmwood.",
-        background: "url('imagens2/img10.jpg')" 
+        background: "url('imagens/img3.jpg')" 
     },
     { 
         question: "Qual médico e especialista em doenças raras ajuda a combater o Drácula?", 
         options: ["Dr. John Seward", "Professor Abraham Van Helsing", "Renfield"], 
         correctAnswer: 1, 
         hint: "Ele é um professor holandês e amigo de Dr. John Seward.",
-        background: "url('imagens2/img11.jpg')" 
+        background: "url('imagens/img4.jpg')" 
     },
     { 
-        question: "Quem é o autor do livro Frankenstein?", 
+        question: "Quem é o autor do livro 'Frankenstein'?", 
         options: ["Bram Stoker", "Mary Shelley", "Edgar Allan Poe"], 
         correctAnswer: 1, 
         hint: "Esta autora é uma escritora inglesa que escreveu esta obra aos 18 anos.",
-        background: "url('imagens2/img12.jpg')" 
+        background: "url('imagens/img5.jpg')" 
     },
     { 
-        question: "Qual é o nome do cientista que cria o monstro em Frankenstein?", 
+        question: "Qual é o nome do cientista que cria o monstro em 'Frankenstein'?", 
         options: ["Henry Clerval", "Robert Walton", "Victor Frankenstein"], 
         correctAnswer: 2, 
         hint: "Ele é o personagem principal e sua criação dá nome ao livro.",
-        background: "url('imagens2/img14.jpg')" 
+        background: "url('imagens/img6.jpg')" 
     },
     { 
-        question: "Como o monstro é frequentemente referido no livro Frankenstein?", 
+        question: "Como o monstro é frequentemente referido no livro 'Frankenstein'?", 
         options: ["A Criatura", "O Demônio", "O Fantasma"], 
         correctAnswer: 0, 
         hint: "Este termo é usado para destacar a ausência de nome próprio.",
-        background: "url('imagens2/img23.jpg')" 
+        background: "url('imagens/img7.jpg')" 
     },
     { 
         question: "Onde Victor Frankenstein realiza a criação do monstro?", 
         options: ["Em seu laboratório em Londres", "Em um laboratório em Ingolstadt", "Em sua casa na Suíça"], 
         correctAnswer: 1, 
         hint: "Esta cidade está localizada na Alemanha e é onde Victor estudava.",
-        background: "url('imagens2/img25.jpg')" 
+        background: "url('imagens/img8.jpg')" 
     },
     { 
         question: "Qual é o motivo principal que leva Victor Frankenstein a criar o monstro?", 
         options: ["Ambição científica e desejo de superar a morte", "Vingança contra seus colegas de faculdade", "Pressão de sua família para ser bem-sucedido"], 
         correctAnswer: 0, 
         hint: "Ele está motivado por um desejo de descobrir os segredos da vida e da morte.",
-        background: "url('imagens2/img26.jpg')" 
+        background: "url('imagens/img9.jpg')" 
     },
     { 
-        question: "Quem é o autor do livro Alice no País das Maravilhas?", 
+        question: "Quem é o autor do livro 'Alice no País das Maravilhas'?", 
         options: ["J.R.R. Tolkien", "Lewis Carroll", "C.S. Lewis"], 
         correctAnswer: 1, 
         hint: "O autor é conhecido pelo seu verdadeiro nome, Charles Lutwidge Dodgson.",
-        background: "url('imagens2/img27.jpg')" 
+        background: "url('imagens/img36.jpg')" 
     },
     { 
         question: "Qual animal Alice segue para cair na toca e chegar ao País das Maravilhas?", 
         options: ["Um coelho", "Um gato", "Um rato"], 
         correctAnswer: 0, 
         hint: "Este animal está sempre olhando para o relógio e dizendo que está atrasado.",
-        background: "url('imagens2/img28.jpg')"
+        background: "url('imagens/img11.jpg')"
     },
     { 
         question: "Qual é o nome do gato que Alice encontra no País das Maravilhas?", 
         options: [" Gato de Botas", "Gato Cheshire", "Gato Félix"], 
         correctAnswer: 1, 
         hint: "Este gato é conhecido por seu sorriso enigmático.",
-        background: "url('imagens2/img30.jpg')"
+        background: "url('imagens/img12.jpg')"
     },
     { 
         question: "Qual personagem é conhecida por dizer 'Cortem-lhe a cabeça!'?", 
         options: ["Rainha Vermelha", "Rainha Branca", "Rainha de Copas"], 
         correctAnswer: 2, 
         hint: "Este gato é conhecido por seu sorriso enigmático.",
-        background: "url('imagens2/img31novo.jpg')"
+        background: "url('imagens/img13.jpg')"
     },
     { 
-        question: "O que Alice come ou bebe para mudar de tamanho?", 
+        question: "O que Alice come ou bebe para mudar de tamanho no livro?", 
         options: ["Uma maçã e uma taça de vinho", "Um bolo e uma poção", "Uma cenoura e um copo de leite"], 
         correctAnswer: 1, 
         hint: "Ela encontra uma garrafa com um rótulo 'Beba-me' e um bolo com um rótulo 'Coma-me'.",
-        background: "url('imagens2/img32.jpg')"
+        background: "url('imagens/img14.jpg')"
     },
     { 
         question: "Quem é o autor do conto O Chamado de Cthulhu?", 
         options: ["Edgar Allan Poe", "H. P. Lovecraft", "Arthur Conan Doyle"], 
         correctAnswer: 1, 
         hint: "O autor é conhecido por suas histórias de terror cósmico e mitos antigos.",
-        background: "url('imagens2/img35novo.jpg')"
+        background: "url('imagens/img15.jpg')"
     },
     { 
         question: "Qual é o nome da criatura principal mencionada no conto O Chamado de Cthulhu?", 
         options: ["Nyarlathotep", "Cthulhu", "Azathoth"], 
         correctAnswer: 1, 
         hint: "Esta criatura é descrita como uma entidade cósmica com características de polvo e dragão.",
-        background: "url('imagens2/img4.jpg')"
+        background: "url('imagens/img16.jpg')"
     },
     { 
         question: "Em qual cidade fictícia se encontram os registros sobre Cthulhu?", 
         options: ["Innsmouth", "Dunwich", "R'lyeh"], 
         correctAnswer: 2, 
         hint: "Esta cidade é submersa e onde Cthulhu está adormecido.",
-        background: "url('imagens2/img7.jpg')" 
+        background: "url('imagens/img17.jpg')" 
     },
     { 
         question: "Qual é o nome do professor que investiga os cultos e as lendas de Cthulhu?", 
         options: ["Professor Armitage", "Professor Angell", "Professor Wilmarth"], 
         correctAnswer: 1, 
         hint: "Ele é um professor da Universidade Brown que encontra um estranho ídolo de Cthulhu.",
-        background: "url('imagens2/img8.jpg')" 
+        background: "url('imagens/img56.jpg')" 
     },
     { 
         question: "Em que ano foi publicado o conto O Chamado de Cthulhu?", 
         options: ["1928", "1936", "1941"], 
         correctAnswer: 0, 
         hint: "O conto foi publicado na revista 'Weird Tales' durante a década de 1920.",
-        background: "url('imagens2/img9.jpg')" 
+        background: "url('imagens/img19.jpg')" 
     },
     { 
         question: "Quem é o autor do livro O Mágico de Oz?", 
         options: ["Lewis Carroll", "J.M. Barrie", "L. Frank Baum"], 
         correctAnswer: 2, 
         hint: "O autor é um escritor americano conhecido por sua série de livros sobre a Terra de Oz.",
-        background: "url('imagens2/img13.jpg')" 
+        background: "url('imagens/img41.jpg')" 
     },
     { 
         question: "Qual é o nome da protagonista que viaja para a Terra de Oz?", 
         options: ["Alice", "Wendy", "Dorothy"], 
         correctAnswer: 2, 
         hint: "Ela é uma garota do Kansas que viaja com seu cachorro Toto.",
-        background: "url('imagens2/img15.jpg')" 
+        background: "url('imagens/img21.jpg')" 
     },
     { 
         question: "Qual é o nome da estrada que Dorothy segue para encontrar o Mágico de Oz?", 
         options: ["Estrada de Tijolos Vermelhos", "Estrada de Tijolos Verdes", "Estrada de Tijolos Amarelos"], 
         correctAnswer: 2, 
         hint: "Esta estrada é conhecida por sua cor vibrante que leva à Cidade das Esmeraldas.",
-        background: "url('imagens2/img16.jpg')" 
+        background: "url('imagens/img22.jpg')" 
     },
     { 
         question: "Quem Dorothy encontra primeiro em sua jornada pela Terra de Oz?", 
         options: ["O Espantalho", "O Homem de Lata", "O Leão Covarde"], 
         correctAnswer: 0, 
         hint: "Este personagem deseja ter um cérebro.",
-        background: "url('imagens2/img17.jpg')" 
+        background: "url('imagens/img23.jpg')" 
     },
     { 
         question: "Qual é o desejo do Leão Covarde no livro O Mágico de Oz?", 
         options: ["Ter um coração", "Ter coragem", "Ter um cérebro"], 
         correctAnswer: 1, 
         hint: "Este personagem deseja ser valente e destemido.",
-        background: "url('imagens2/img18.jpg')" 
+        background: "url('imagens/img40.jpg')" 
     },
     { 
         question: "Quem é o autor do livro Psicose?", 
         options: ["Stephen King", "Robert Bloch", "Alfred Hitchcock"], 
         correctAnswer: 1, 
         hint: "O autor é um escritor americano famoso por suas histórias de terror e suspense.",
-        background: "url('imagens2/img19.jpg')" 
+        background: "url('imagens/img26.jpg')" 
     },
     { 
         question: "Qual é o nome do protagonista que gerencia o Bates Motel?", 
         options: ["Norman Bates", "Sam Loomis", "Marion Crane"], 
         correctAnswer: 0, 
         hint: "Este personagem tem uma relação complexa com sua mãe.",
-        background: "url('imagens2/img33novo.jpg')"
+        background: "url('imagens/img27.jpg')"
     },
     { 
         question: "Quem é a primeira vítima que chega ao Bates Motel no livro Psicose?", 
         options: ["Lila Crane", "Marion Crane", "Arbogast"], 
         correctAnswer: 1, 
         hint: "Esta personagem rouba uma grande quantia de dinheiro antes de chegar ao motel.",
-        background: "url('imagens2/img21.jpg')"
+        background: "url('imagens/img28.jpg')"
     },
     { 
         question: "Qual é a relação de Norman Bates com sua mãe no livro Psicose?", 
         options: ["Ela está morta, mas ele mantém seu corpo preservado", "Ela é sua cúmplice nos assassinatos", "Ela o abandonou na infância"], 
         correctAnswer: 0, 
         hint: "A mãe de Norman tem uma presença inquietante apesar de seu estado.",
-        background: "url('imagens2/img22.jpg')"
+        background: "url('imagens/img29.jpg')"
     },
     { 
         question: "Em que ano foi publicado o livro Psicose?", 
         options: ["1959", "1965", "1971"], 
         correctAnswer: 0, 
         hint: "O livro foi publicado no final da década de 1950 e inspirou um famoso filme de Alfred Hitchcock.",
-        background: "url('imagens2/img24.jpg')"
+        background: "url('imagens/img30.jpg')"
     },
     { 
         question: "Quem é o autor do livro A Revolução dos Bichos?", 
         options: ["Aldous Huxley", "George Orwell", "Ray Bradbury"], 
         correctAnswer: 1, 
         hint: "O autor é conhecido também pelo livro '1984'.",
-        background: "url('imagens2/img29novo.jpg')" 
+        background: "url('imagens/img31.jpg')" 
     },
     { 
         question: "Qual é o nome da fazenda onde se passa a história do livro A Revolução dos Bichos?", 
-        options: ["Fazenda Manor", "Fazenda Green", " Fazenda Sunny"], 
+        options: ["Fazenda Manor", "Fazenda Green", "Fazenda Sunny"], 
         correctAnswer: 0, 
         hint: "A fazenda tem um nome associado a propriedade tradicional antes da revolução.",
-        background: "url('imagens2/img36.jpg')"
+        background: "url('imagens/img32.jpg')"
     },
     { 
         question: "Qual porco lidera a revolução contra os humanos no livro A Revolução dos Bichos?", 
         options: ["Napoleão", "Bola-de-Neve", "Garganta"], 
         correctAnswer: 0, 
         hint: "Este porco se torna o líder autoritário após a revolução.",
-        background: "url('imagens2/img37.jpg')"
+        background: "url('imagens/img42.jpg')"
     },
     { 
         question: "Qual é o lema que os animais adotam após a revolução no livro de George Orwell?", 
         options: ["Trabalhar e prosperar", "Todos os animais são iguais", "Liberdade e fraternidade"], 
         correctAnswer: 1, 
         hint: "Este lema é modificado ao longo da história para refletir a corrupção do poder.",
-        background: "url('imagens2/img38.jpg')"
+        background: "url('imagens/img39.jpg')"
     },
     { 
         question: "Qual personagem representa a propaganda e manipulação da verdade na fazenda do livro de George Orwell?", 
         options: ["Sansão", "Garganta", "Minimus"], 
         correctAnswer: 1, 
         hint: "Este personagem é um porco conhecido por sua habilidade em discursos persuasivos.",
-        background: "url('imagens2/img39.jpg')"
+        background: "url('imagens/img35.jpg')"
+    },
+    { 
+        question: "Quem é o autor do conto O Gato Preto?", 
+        options: ["H.P. Lovecraft", "Edgar Allan Poe", "Nathaniel Hawthorne"], 
+        correctAnswer: 1, 
+        hint: "O autor é conhecido como um mestre do horror gótico e escreveu 'O Corvo'.",
+        background: "url('imagens/img59.jpg')" 
+    },
+    {
+        question: "Qual é o nome do primeiro gato no conto O Gato Preto?", 
+        options: ["Pluto", "Mercury", "Apollo"], 
+        correctAnswer: 0, 
+        hint: "O nome do gato é também o nome do deus romano do submundo.",
+        background: "url('imagens/img65.jpg')"
+    },
+    { 
+        question: "O que o narrador faz ao gato em um acesso de fúria no conto O Gato Preto?", 
+        options: ["Abandona o gato", " Cega o gato de um olho", "Alimenta o gato com veneno"], 
+        correctAnswer: 1, 
+        hint: "O narrador comete um ato cruel que envolve um dos olhos do gato.",
+        background: "url('imagens/img67.jpg')"
+    },
+    { 
+        question: "Como o narrador esconde o corpo de sua esposa depois de matá-la no conto O Gato Preto?", 
+        options: ["Enterra no jardim", "Joga no rio", "Empareda no porão"], 
+        correctAnswer: 2, 
+        hint: "Ele usa uma técnica de construção para ocultar o corpo.",
+        background: "url('imagens/img49.jpg')"
+    },
+    { 
+        question: "Como a polícia descobre o corpo da esposa do narrador no conto O Gato Preto de Allan Poe?", 
+        options: ["Um vizinho vê o crime", "O gato mia de dentro da parede", "O narrador confessas"], 
+        correctAnswer: 1, 
+        hint: "Um som vindo do local onde o corpo está escondido leva a polícia até ele.",
+        background: "url('imagens/img50.jpg')"
     },
 ];
 
@@ -257,7 +292,7 @@ document.addEventListener("DOMContentLoaded", () => {
     loadQuestion();
 });
 
-document.body.style.backgroundImage = questions[0].background;
+document.body.style.backgroundImage = "url('imagens/img25.jpg')"; /*questions[0].background*/
 
 function startGame() {
     playerName = document.getElementById("player-name").value;
@@ -302,7 +337,7 @@ function loadQuestion() {
     questionTimeout = setTimeout(() => {
         currentQuestionIndex++;
         loadQuestion();
-    }, 15000); // Move to the next question after 15 seconds
+    }, 15000); // Move para a próxima pergunta após 15 segundos
 
     updateHintButtons();
 }
@@ -318,7 +353,7 @@ function checkAnswer(selectedOption) {
         resultContainer.style.color = "red";
     }
     currentQuestionIndex++;
-    setTimeout(loadQuestion, 1000); // Move to the next question after 1 second
+    setTimeout(loadQuestion, 1000); //Move para a próxima pergunta após 1 segundo do resultado correto ou errado
 }
 
 function useHint(hintNumber) {
@@ -362,7 +397,7 @@ function showQuestionAgain() {
         questionTimeout = setTimeout(() => {
             currentQuestionIndex++;
             loadQuestion();
-        }, 5000); // Move to the next question after 5 seconds
+        }, 10000); // Move to the next question after 10 seconds
     }, 5000); // Show the question again for 5 seconds
 }
 
